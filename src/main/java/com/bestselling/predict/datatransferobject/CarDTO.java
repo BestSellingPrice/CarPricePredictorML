4fbc7ef7f5292abecec6cbe12fce660b3568ab68
kirankvs <kalasandramkiran@gmail.com>
2018-10-16 15:56:58 +0530

    closes #11 issue.. CarDTO default values not initialized with Lombok

--- src/main/java/com/bestselling/predict/datatransferobject/CarDTO.java
+++ src/main/java/com/bestselling/predict/datatransferobject/CarDTO.java
@@ -13,24 +13,36 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 @Data
 public class CarDTO {
+	
 
-    int kilometers;
-    int owner;
-    RCType rcType = RCType.INDIVIDUAL;
-    EngineType engineType = EngineType.PETROL;
-    TransmissionType transmissionType = TransmissionType.MANUAL;
-    Date registrationyear = new Date(100, 1, 1);
-    String city = "Bangalore";
-    ModelType modelType = ModelType.LXI;
-    double price;
+   int kilometers;
+   int owner;
+   RCType rcType ;
+   EngineType engineType ;
+   TransmissionType transmissionType ;
+   Date registrationyear;
+   String city ;
+   ModelType modelType ;
+   
+   double price;
+      
 
-    @JsonFormat(shape = JsonFormat.Shape.ANY, pattern = "yyyy", timezone = "IST")
-    public Date getRegistrationyear() {
-        return registrationyear;
-    }
+   @JsonFormat(shape = JsonFormat.Shape.ANY, pattern = "yyyy", timezone = "IST")
+   public Date getRegistrationyear() {
+       return registrationyear;
+   }
 
+   
+   public static class CarDTOBuilder
+   {
+   	RCType rcType = RCType.INDIVIDUAL;
+       EngineType engineType = EngineType.PETROL;
+       TransmissionType transmissionType = TransmissionType.MANUAL;
+       Date registrationyear = new Date(100, 1, 1);
+       String city = "Bangalore";
+       ModelType modelType = ModelType.LXI;
+   }
 }
